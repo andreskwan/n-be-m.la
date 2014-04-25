90b67f85d00d90a1ef2c94cfd56610fdbc9ba11c
ANDRES KWAN <andreskwan@gmail.com>
2014-04-25 08:49:14 -0500

    6.7 - tomando imagen del profile de twitter - ok

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -1,7 +1,7 @@
 // todo lo relacionado con la ruta app
 var appController = function (server,users) {
 
-	
+
 	var isntLoggedIn = function (req, res, next) {
 		// debugger;
 		if(!req.session.passport.user){
@@ -13,8 +13,14 @@ var appController = function (server,users) {
 	};
 
 	server.get('/app', isntLoggedIn, function (req, res) {
-		// debugger;
+		debugger;
 		res.render('app', {			
+			//user es el objeto json o profile del usuario
+			//no seria mejor que en app.js 
+			//sacar al info a la db y luego pasarla al
+			//browser
+			//supongo que asi es mas rapido, por que 
+			//no tiene que ir hasta el servidor para tomar acciones
 			user : req.session.passport.user,
 			users : users 
 		});
